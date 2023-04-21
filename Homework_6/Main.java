b2345f3d4dfa6bcab2c7cfa270c09e108fe2060c
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-04-21 14:28:50 +0400

    Have not time anymore

--- Homework_6/Main.java
+++ Homework_6/Main.java
@@ -1,5 +1,6 @@
 package Homework_6;
 
+import Homework_6.Tank.LiquidTank;
 import Homework_6.Tank.SolidTank;
 
 /**
@@ -14,7 +15,16 @@ public class Main {
         eatMachine.addProduct(new Eat("Mars", 9.99f, 0.08f));
         eatMachine.addProduct(new Eat("Snikers", 8.5f, 0.08f));
     
-        View UI = new View();
-        UI.run(eatMachine);
+        Controller<Drink> drinkMachine = new Controller<>(new LiquidTank(12.0f));
+        drinkMachine.addProduct(new Drink("Coffee", 7.50f, 0.3f));
+        drinkMachine.addProduct(new Drink("Moloko", 8.50f, 0.5f));
+        drinkMachine.addProduct(new Drink("Vodka", 3.50f, 0.5f));
+
+
+        //View UI = new View();
+        //UI.run(eatMachine);
+
+        View UI2 = new View();
+        UI2.run(drinkMachine);
     }
 }
